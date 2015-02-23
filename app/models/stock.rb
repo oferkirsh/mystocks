1038c3548b0ac1303f9324dc909d0cff74c6b183
ofer <ofer@ofer-VirtualBox.(none)>
2015-02-23 09:03:30 +0200

    add pg

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,6 @@
 class Stock < ActiveRecord::Base
 	attr_accessor :last_trade_price, :ask_real_time
 	has_many :transactions
+	validates :symbol, presence: true
+	validates :symbol, uniqueness: true
 end
